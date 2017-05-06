c20a4a4ff84f0f2c808aa2b350de97ee85252b3e
Haiyu Zhu <zhuhaiyu1991@163.com>
2017-05-06 10:04:51 +0800

    convert *.pdf to *.eps

--- make.bat
+++ make.bat
@@ -1,10 +1,10 @@
 del /Q *.aux *.toc *.lon *.lor *.lof *.ilg *.idx *.ind *.out *.log *.exa
 del /Q *.nav *.snm *.bbl *.blg *.spl *.lot *.bak *~ *.dvi *.ps
 set ARTICLE=template
-latex --enable-pipes %ARTICLE%.tex
+latex %ARTICLE%.tex
 bibtex %ARTICLE%
-latex --enable-pipes %ARTICLE%.tex
+latex %ARTICLE%.tex
 gbk2uni %ARTICLE%
-latex --enable-pipes %ARTICLE%.tex
+latex %ARTICLE%.tex
 dvipdfmx %ARTICLE%.dvi
 start "" "C:\Program Files\SumatraPDF\SumatraPDF.exe" ./%ARTICLE%.pdf
