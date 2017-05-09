02696ff65b5cacb59491c68b21547ba4b75c540f
Haiyu Zhu <zhuhaiyu1991@163.com>
2017-05-09 15:43:42 +0800

    修改图片大小为A4

--- make.bat
+++ make.bat
@@ -6,5 +6,5 @@ bibtex %ARTICLE%
 latex %ARTICLE%.tex
 gbk2uni %ARTICLE%
 latex %ARTICLE%.tex
-dvipdfmx %ARTICLE%.dvi
+dvipdfm %ARTICLE%.dvi
 start "" "C:\Program Files\SumatraPDF\SumatraPDF.exe" ./%ARTICLE%.pdf
